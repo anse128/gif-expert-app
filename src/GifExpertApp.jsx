e5a15109fbf6d7fb4115304a68cffdfef42bebb7
anse128 <sebastianescobarosorio@gmail.com>
2024-05-16 16:03:06 -0500

    componente GifGrid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,37 +1,36 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
 
     const onAddCategory = (newCategory) => {
-        if(categories.includes(newCategory)) return
+        if (categories.includes(newCategory)) return
         setCategories([newCategory, ...categories])
         console.log(newCategory)
     }
 
     return (
         <>
-            {/* titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
             <AddCategory
                 // setCategories={setCategories}
                 onNewCategory={value => onAddCategory(value)}
                 currentCategories={categories}
             />
 
-            {/* Listado de Gifs */}
-            {/* <button onClick={onAddCategory}>Agregar</button> */}
-            <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
+            {
+                categories.map((category) => (
+                    // <GifGrid /> react ya sabe que tiene un key = a category
+                    <GifGrid
+                        key={category}
+                        category={category} />
+                ))
+            }
 
-            {/* Gif Item */}
         </>
     )
 }
