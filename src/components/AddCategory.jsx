7ffeb36ebe4a1324a98ef2e4c2db905fee4754b6
anse128 <sebastianescobarosorio@gmail.com>
2024-05-24 23:54:27 -0500

    ....

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -28,7 +28,6 @@ export const AddCategory = ({ onNewCategory }) => {
                 value={inputValue}
                 // onChange={(event) => onInputChange(event)}
                 onChange={onInputChange}
-            //.
             />
         </form>
     )
